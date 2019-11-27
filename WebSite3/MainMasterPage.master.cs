ec2d2a020ff39b53bf2ed89d98c0037bee8ac2bd
SELJER <SELJER@SELJER>
2019-11-27 17:24:35 +0200

    final v3

--- WebSite3/MainMasterPage.master.cs
+++ WebSite3/MainMasterPage.master.cs
@@ -9,7 +9,7 @@ public partial class MainMasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //
     }
 
     protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
